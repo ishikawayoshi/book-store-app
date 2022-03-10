77d3706e1ced3c94f02b4d6bc289d2629413b63d
Shpak Aleksandr <shpak.aleks@gmail.com>
2022-03-11 00:58:38 +0400

    initial commit

---
+++ src/main/java/com/example/bookstoreapp/configuration/BooksStoreConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.bookstoreapp.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BooksStoreConfiguration {
+
+    // public DataSource getDataSource(){
+    //     DataSource ds = new DataSource();
+    // }
+    
+}
