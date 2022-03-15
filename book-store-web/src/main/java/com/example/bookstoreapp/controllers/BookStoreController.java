6b7515f06b2ac487e2e09bcf5536161331f2bbff
Shpak Aleksandr <shpak.aleks@gmail.com>
2022-03-16 02:25:34 +0400

    update book-store-ui angular project, add frontend plugin,resource copy/delete plugin

--- book-store-web/src/main/java/com/example/bookstoreapp/controllers/BookStoreController.java
+++ book-store-web/src/main/java/com/example/bookstoreapp/controllers/BookStoreController.java
@@ -2,17 +2,17 @@ package com.example.bookstoreapp.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-// @RequestMapping("/api/v1/books")
+@Controller
 public class BookStoreController {
 
     @GetMapping
-    public ResponseEntity<String> getSimpleResponse(){
-        return new ResponseEntity<>("OK",HttpStatus.OK);
+    public String getSimpleResponse(){
+        return "index";
     }
     
 }
